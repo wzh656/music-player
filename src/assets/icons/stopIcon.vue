7bf04a54e551feb9536b8d5f7621e7c4fb27248b
wzh656 <1826632591@qq.com>
2024-08-04 10:01:09 +0800

    feat(icon): svg -> vue

---
+++ src/assets/icons/stopIcon.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Generator: $$$/GeneralStr/196=Adobe Illustrator 27.6.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <svg
+    version="1.1"
+    id="图层_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 256 256"
+    style="enable-background: new 0 0 256 256"
+    xml:space="preserve"
+  >
+    <g>
+      <line class="st3" x1="89.25" y1="200" x2="89.25" y2="56" />
+      <line class="st3" x1="166.75" y1="200" x2="166.75" y2="56" />
+    </g>
+  </svg>
+</template>
+
+<style type="text/css" scoped>
+.st0 {
+  fill: #cccccc;
+}
+.st1 {
+  fill: none;
+  stroke: #cccccc;
+  stroke-width: 10;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+.st2 {
+  fill: #cccccc;
+  stroke: #cccccc;
+  stroke-width: 10;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+.st3 {
+  fill: none;
+  stroke: #cccccc;
+  stroke-width: 15;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+</style>
---
+++ src/assets/icons/unmaximizeIcon.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- Generator: $$$/GeneralStr/196=Adobe Illustrator 27.6.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <svg
+    version="1.1"
+    id="图层_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 256 256"
+    style="enable-background: new 0 0 256 256"
+    xml:space="preserve"
+  >
+    <g>
+      <g>
+        <path
+          class="st0"
+          d="M207,43c1.08,0,2,0.92,2,2v112c0,1.08-0.92,2-2,2h-34.57V90.97c0-9.1-7.41-16.51-16.51-16.51H93V45
+			c0-1.08,0.92-2,2-2H207 M207,33H95c-6.6,0-12,5.4-12,12v39.46h72.92c3.59,0,6.51,2.91,6.51,6.51V169H207c6.6,0,12-5.4,12-12V45
+			C219,38.4,213.6,33,207,33L207,33z"
+        />
+      </g>
+      <g>
+        <path
+          class="st0"
+          d="M158,86c1.08,0,2,0.92,2,2v112c0,1.08-0.92,2-2,2H46c-1.08,0-2-0.92-2-2V88c0-1.08,0.92-2,2-2H158 M158,76H46
+			c-6.6,0-12,5.4-12,12v112c0,6.6,5.4,12,12,12h112c6.6,0,12-5.4,12-12V88C170,81.4,164.6,76,158,76L158,76z"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<style type="text/css" scoped>
+.st0 {
+  fill: #cccccc;
+}
+.st1 {
+  fill: none;
+  stroke: #cccccc;
+  stroke-width: 10;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+.st2 {
+  fill: #cccccc;
+  stroke: #cccccc;
+  stroke-width: 10;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+.st3 {
+  fill: none;
+  stroke: #cccccc;
+  stroke-width: 15;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
+}
+</style>
